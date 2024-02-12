bf8ea5d84329bed02e8caaacfdf9149149ec2727
samishal1998 <samishal.1998@gmail.com>
2024-02-12 15:28:08 +0200

    fix(tailwind): fixed the missing css import in storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,8 @@
 import type { Preview } from "@storybook/react";
 import {themes} from "@storybook/theming";
 import theme from "./theme";
+import '../src/index.css'
+import '../src/App.css'
 
 const preview: Preview = {
   parameters: {
