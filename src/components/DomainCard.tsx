9c93323fd5bc4f48f1d86f0c3d016d4161c4c6ba
samishal1998 <samishal.1998@gmail.com>
2024-02-12 02:32:16 +0200

    fix(components): fixed DomainCard component to have more flexibility in controlling the view and fixed the stories

--- src/components/DomainCard.tsx
+++ src/components/DomainCard.tsx
@@ -1,50 +1,83 @@
-import React, { useMemo } from 'react'
+import React, {useMemo} from 'react'
+import moment from "moment";
+import {ShortAddress} from "@components/ShortAddress";
 
-export type DomainCardProps = Partial<{
-    userAddress,
-    domainName: string,
-    loading?:boolean,
-    isOwned?: boolean,
-    isAvailable?: boolean,
-    isTaken?: boolean,
-    isOwnedOrTaken: boolean
-}>
+export type DomainCardProps =
+    {
+        domainName?: string,
+        loading?: boolean,
+        showExpiration?: boolean,
+        expirationDate?: Date,
+    } & (
+    {
+        ownerAddress?: string,
+        showOwner?: boolean,
+        sameOwner?: boolean,
+        onSettingsClicked?: ()=> any;
+        status: 'TAKEN', // shouldn't be derived from ownerAddress in case the user doesn't want to pass the owner address
+    } | {
+    status: 'AVAILABLE'
+})
 
-export const DomainCard = ({userAddress, domainName, loading, isAvailable, isOwned, isTaken, isOwnedOrTaken}: DomainCardProps) => {
+export const DomainCard = (props: DomainCardProps) => {
 
-    const shortUserAddress = useMemo(() => {
-        if (!userAddress) return
-        return <>{userAddress.slice(0, 7)}
-            &bull;&bull;&bull;&bull;
-            {userAddress?.slice(37, 42)}</>
-    }, [userAddress]);
+    const {showOwner, sameOwner, ownerAddress, onSettingsClicked} = useMemo(() => {
+        if (props.status === 'TAKEN') {
+            return {
+                showOwner: props.showOwner,
+                sameOwner: props.sameOwner,
+                ownerAddress: props.ownerAddress,
+                onSettingsClicked: props.onSettingsClicked,
+            }
+        }
+        return {}
 
-  return (
-    <div className='relative rounded-[20px] p-6 bg-black flex flex-col justify-between items-center w-[400px] h-[400px] rounded-[20px] drop-shadow-[0_0_15px_rgba(202,202,202,0.25)]'>
-        <div className='flex w-full h-[30px] items-center justify-between'>
-            <img className='max-w-[163px]' src={isOwned? "/assets/namefiapp.svg": isTaken? "/assets/Taken.svg": "/assets/Available.svg"} alt="namefiapp img" role='img' />
-            <button className='relative block'>
-                {isOwned && <img className='w-[30px]' src="/assets/Gear.svg" alt="Gear image" role='img' />}
-            </button>
-        </div>
-        <h3 className='text-white font-semibold text-[28px] tracking-[0.1em]'>{domainName}</h3>
-        <div className='w-full h-[36px] flex justify-between '>
-            {isOwnedOrTaken && 
+    }, [props]);
+    const {
+        domainName,
+        loading,
+        showExpiration,
+        expirationDate,
+        status,
+    } = props
+
+
+
+    return (
+        <div
+            className='relative rounded-[20px] p-6 bg-black flex flex-col justify-between items-center w-[400px] h-[400px] rounded-[20px] drop-shadow-[0_0_15px_rgba(202,202,202,0.25)]'>
+            <div className='flex w-full h-[30px] items-center justify-between'>
+                <img className='max-w-[163px]'
+                     src={status==='TAKEN'? (sameOwner ?"/assets/namefiapp.svg" :"/assets/Taken.svg") : "/assets/Available.svg"}
+                     alt="namefiapp img" role='img'/>
+                <button className='relative block' onClick={onSettingsClicked}>
+                    { sameOwner &&  <img className='w-[30px]' src="/assets/Gear.svg" alt="Gear image" role='img'/>}
+                </button>
+            </div>
+            <h3 className='text-white font-semibold text-[28px] tracking-[0.1em]'>{domainName}</h3>
+            <div className='w-full h-[36px] flex justify-between '>
                 <div className='w-full h-full flex justify-between '>
                     <div className='min-w-[106px] h-full'>
-                        <p className='text-[#acacac] text-xs tracking-[0.1em] '>Owned by</p>
-                        <p className='text-[#ededed] text-sm tracking-[0.1em] '>{shortUserAddress}</p>
+                        {showOwner && !!ownerAddress &&
+                            <>
+                                <p className='text-[#acacac] text-xs tracking-[0.1em] '>Owned by</p>
+                                <p className='text-[#ededed] text-sm tracking-[0.1em] '><ShortAddress address={ownerAddress}/></p>
+                            </>
+                        }
                     </div>
                     <div className='w-[150px] h-full flex items-center justify-between '>
-                        <img className='w-[36.75px]' src="/assets/lock.svg" alt="lock image" role='img' />
-                        <div className='w-[99px]'>
-                        <p className='text-[#acacac] text-[11px] font-bold tracking-[0.1em] '>Expires on</p>
-                        <p className='text-[#ededed] text-sm font-bold tracking-[0.1em] '>Mar 01 2025</p>
-                        </div>
+                        {showExpiration && !!expirationDate &&
+                            <>
+                                <img className='w-[36.75px]' src="/assets/lock.svg" alt="lock image" role='img'/>
+                                <div className='w-[99px]'>
+                                    <p className='text-[#acacac] text-[11px] font-bold tracking-[0.1em] '>Expires on</p>
+                                    <p className='text-[#ededed] text-sm font-bold tracking-[0.1em] '>{moment(expirationDate).format('MMM DD YYYY')}</p>
+                                </div>
+                            </>
+                        }
                     </div>
                 </div>
-            }
+            </div>
         </div>
-    </div>
-  )
+    )
 }
--- src/stories/Components/DomainCard.stories.tsx
+++ src/stories/Components/DomainCard.stories.tsx
@@ -1,10 +1,14 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { DomainCard } from "@components/DomainCard";
+import moment from "moment";
 
 const meta = {
     title: 'Components/DomainCard',
     component: DomainCard,
     tags: ['autodocs'],
+    argTypes: {
+        onSettingsClicked: {action: 'onSettingsClicked'}
+    },
     parameters: {
         layout: 'centered'
     }
@@ -13,30 +17,34 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>
 
-export const OwnedDomainCard: Story = {
-    args: {
-        domainName: 'namefi.eth',
-        userAddress: '0xfdB5929A4a3e5a98D70c76E86681cFfdfbf47Fe4',
-        loading: false,
-        isOwned: true,
-        isOwnedOrTaken: true
-    }
-}
+
 export const DomainAvailableCard: Story = {
     args: {
         domainName: 'namefi.io',
-        userAddress: '',
-        loading: false,
-        isAvailable: true,
-        isOwnedOrTaken: false
+        status: 'AVAILABLE',
     }
 }
+
 export const DomainTakenCard: Story = {
     args: {
         domainName: 'namefi.com',
-        userAddress: '0xfdB5929A4a3e5a98D70c76E86681cFfdfbf47Fe4',
-        loading: false,
-        isTaken: true,
-        isOwnedOrTaken: true
+        ownerAddress: '0xfdB5929A4a3e5a98D70c76E86681cFfdfbf47Fe4',
+        status: 'TAKEN',
+        showOwner:true,
+        showExpiration:true,
+        expirationDate: moment().add(1, 'year').toDate(),
     }
 }
+
+export const OwnedDomainCard: Story = {
+    args: {
+        domainName: 'namefi.eth',
+        ownerAddress: '0xfdB5929A4a3e5a98D70c76E86681cFfdfbf47Fe4',
+        status: 'TAKEN',
+        showOwner:true,
+        sameOwner: true,
+        showExpiration:true,
+        expirationDate: moment().add(1, 'year').toDate(),
+    }
+}
+
