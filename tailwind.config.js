e79b27495ae79233d1ccba1090a195ffaee81ed3
samishal1998 <samishal.1998@gmail.com>
2024-02-12 02:29:52 +0200

    fix(tailwind): fixed config type assignment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
+const colors = require('tailwindcss/colors')
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
 }
