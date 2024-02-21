fe0e1c9ceca4ea9b2beed0e879a59af3145475ad
xinbenlv <zzn-github@zzn.im>
2024-02-21 09:23:03 -0800

    chore(react-lib): unpublish AppLayoutFooter

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
 export { HeaderSearchBar } from './components/HeaderSearchBar';
-export { AppLayoutFooter } from './components/AppLayout/AppLayoutFooter';
-export { AppLayoutFooterLink } from './components/AppLayout/AppLayoutFooterLink';
