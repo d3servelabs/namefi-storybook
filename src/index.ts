91f5a0ecf24608d940fa96ef768057bcdb281d07
xinbenlv <zzn-github@zzn.im>
2024-02-21 09:02:32 -0800

    chore(react-lib): expose AppLayoutFooter

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 export { HeaderSearchBar } from './components/HeaderSearchBar';
+export { AppLayoutFooter } from './components/AppLayout/AppLayoutFooter';
+export { AppLayoutFooterLink } from './components/AppLayout/AppLayoutFooterLink';
