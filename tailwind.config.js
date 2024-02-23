0d654f1c999aa6836aeb51a304e33b1fae216218
samishal1998 <samishal.1998@gmail.com>
2024-02-23 03:07:04 +0200

    feat(tailwind): added extra values to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-
 const colors = require('tailwindcss/colors');
 
 /**
@@ -10,88 +9,86 @@ const colors = require('tailwindcss/colors');
  * @returns {T[]}
  */
 const arrayRange = (start, stop, step, converter) =>
-    Array.from(
-        { length: ((stop - start) / step) + 1 },
-        (value, index) =>{
-          const output = start + index * step;
-          if(!converter) return output;
-          else return converter(output,index)
-        }
-    );
+	Array.from({ length: (stop - start) / step + 1 }, (value, index) => {
+		const output = start + index * step;
+		if (!converter) return output;
+		else return converter(output, index);
+	});
 
 const grayscale = Object.fromEntries(
-    Array(99)
-        .fill(0)
-        .map((value, index, array) => [index + 1, `${index + 1}%`]),
+	Array(99)
+		.fill(0)
+		.map((value, index, array) => [index + 1, `${index + 1}%`]),
 );
 
-const borderRadius = Object.fromEntries(
-    arrayRange(0,100,1,(output)=> ([`rounded-${output}`,`${(output/4).toPrecision(2)}rem`]))
+const baseUnit = Object.fromEntries(
+	arrayRange(0, 25, 0.25, (output) => [output, `${(output / 4).toFixed(4)}rem`]),
 );
-const fontSize = Object.fromEntries(
-    arrayRange(0,100,1,(output)=> ([`text-${output}`,`${(output/4).toPrecision(2)}rem`]))
-);
+console.log(baseUnit)
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {
-      borderRadius,fontSize,
-      fontFamily: {
-        primary: ['Roboto Mono', 'Serif'],
-        secondary: ['Nunito', 'Serif'],
-        third: ['Roboto Flex', 'Serif'],
-      },
-      colors: {
-        warning: '#FF3C3C',
-        primary: {
-          500: '#48E59B',
-          700: '#48e59bcc',
-          900: '#0A4429'
-        },
-        brand: {
-          100: '#c6eedb0d',
-          200: '#c6eedb33',
-          300: '#C6EEDB',
-          dark: '#232C27',
-          black: '#111111',
-          blue: '#70A8F4',
-          green: '#48E59B'
-        },
-        black: {
-          200: '#d9d9d9cc',
-          400: '#797979',
-          500: '#D9D9D9',
-          600: '#313131e6',
-          700: '#111111b3',
-          900: '#111111',
-          1000: '#000000'
-        },
-        border: {
-          500: '#444444'
-        }
-      },
-      keyframes: {
-        loading: {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(100%)' },
-        },
-        dissolve: {
-          '0%': {
-            opacity: 0,
-          },
-          '50%': {
-            opacity: 1
-          }
-        }
-      },
-      animation: {
-        'loading': 'loading 1s ease-in-out infinite alternate',
-        'dissolve': 'dissolve 0.2s linear 1'
-      },
-
-    },
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			borderRadius: baseUnit,
+			fontSize: baseUnit,
+			width: baseUnit,
+			height: baseUnit,
+			inset: baseUnit,
+			padding: baseUnit,
+			fontFamily: {
+				primary: ['Roboto Mono', 'Serif'],
+				secondary: ['Nunito', 'Serif'],
+				third: ['Roboto Flex', 'Serif'],
+			},
+			colors: {
+				warning: '#FF3C3C',
+				primary: {
+					500: '#48E59B',
+					700: '#48e59bcc',
+					900: '#0A4429',
+				},
+				brand: {
+					100: '#c6eedb0d',
+					200: '#c6eedb33',
+					300: '#C6EEDB',
+					dark: '#232C27',
+					black: '#111111',
+					blue: '#70A8F4',
+					green: '#48E59B',
+				},
+				black: {
+					200: '#d9d9d9cc',
+					400: '#797979',
+					500: '#D9D9D9',
+					600: '#313131e6',
+					700: '#111111b3',
+					900: '#111111',
+					1000: '#000000',
+				},
+				border: {
+					500: '#444444',
+				},
+			},
+			keyframes: {
+				loading: {
+					'0%': { transform: 'translateX(0%)' },
+					'100%': { transform: 'translateX(100%)' },
+				},
+				dissolve: {
+					'0%': {
+						opacity: 0,
+					},
+					'50%': {
+						opacity: 1,
+					},
+				},
+			},
+			animation: {
+				loading: 'loading 1s ease-in-out infinite alternate',
+				dissolve: 'dissolve 0.2s linear 1',
+			},
+		},
+	},
+	plugins: [],
+};
--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,37 @@
 /* eslint-disable no-undef */
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path";
-import svgr from "vite-plugin-svgr";
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname,"./src"),
-      '@components': path.resolve(__dirname,"./src/components"),
-      '@assets': path.resolve(__dirname,"./src/assets"),
-      '@pages': path.resolve(__dirname,"./src/pages"),
-      '@stories': path.resolve(__dirname,"./src/stories"),
-      '@utils': path.resolve(__dirname,"./src/utils"),
-    }
-  },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "namefi-ui",
-      fileName: "index",
-    },
-    rollupOptions: {
-      external: ["react"],
-      plugins:[
-        svgr(),
-      ],
-      output: {
-        globals: {
-          'react': 'React'
-        }
-      }
-    },
-  },
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@components': path.resolve(__dirname, './src/components'),
+			'@assets': path.resolve(__dirname, './src/assets'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@stories': path.resolve(__dirname, './src/stories'),
+			'@utils': path.resolve(__dirname, './src/utils'),
+			'@icons': path.resolve(__dirname, './src/components/icons/*'),
+		},
+	},
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, 'src/index.ts'),
+			name: 'namefi-ui',
+			fileName: 'index',
+		},
+		rollupOptions: {
+			external: ['react'],
+			plugins: [svgr()],
+			output: {
+				globals: {
+					react: 'React',
+				},
+			},
+		},
+	},
+});
