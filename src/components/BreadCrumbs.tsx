e6ea19e6c134cecaad9ab64c5d5b0fc0b817cf2d
samishal1998 <samishal.1998@gmail.com>
2024-02-25 16:58:41 +0200

    feat(components): created BreadCrumb Components

---
+++ src/components/BreadCrumbs.tsx
@@ -0,0 +1,48 @@
+import { cn } from '@utils/cn';
+import React, {ReactElement, ReactNode} from 'react';
+import { ChevronRight } from '@web3uikit/icons';
+
+export function BreadCrumbsRoot({
+	children,
+	className,
+	activeIndex = (children?.length??0) - 1,
+}: Omit<React.ComponentProps<'div'>,'children'> & {
+	activeIndex?: number;
+	children?: ReactElement[];
+}) {
+	return (
+		<div className={cn('flex flex-row gap-2', className)}>
+			{children?.map((crumb, index) => {
+
+				return (
+					<>
+						<ChevronRight className={'w-4.5 h-4.5 text-[#48E59B]'} />
+						{React.cloneElement(crumb, { active: index === activeIndex })}
+					</>
+				);
+			})}
+		</div>
+	);
+}
+
+export function Crumb({
+	children,
+	active,
+	className,
+	...props
+}: React.ComponentProps<'a'> & { active?: boolean }) {
+	return (
+		<a
+			{...props}
+			className={cn(
+				"font-['Roboto_Mono'] font-3.5 text-[#48E59B]/50 hover:text-[#48E59B]/80 active:text-[#48E59B] hover:underline ",
+				active && 'text-[#48E59B] font-medium',
+				className,
+			)}>
+			{children}
+		</a>
+	);
+}
+
+BreadCrumbsRoot.Crumb = Crumb;
+export default BreadCrumbsRoot;
