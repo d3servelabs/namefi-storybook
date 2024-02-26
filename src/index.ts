4fdb783822e289b5035476d79b3468628f20886a
samishal1998 <samishal.1998@gmail.com>
2024-02-27 00:40:33 +0200

    fix(package): limit css imports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import BaseLayout from '@components/Layouts/BaseLayout';
-import UserAuthState from '@components/UserAuthState';
-import OwnedDomains from '@components/OwnedDomains';
-import DomainSettings from '@components/DomainSettings';
-import './index.css';
+export { default as BaseLayout } from '@components/Layouts/BaseLayout';
+export { default as UserAuthState } from '@components/UserAuthState';
+export { default as OwnedDomains } from '@components/OwnedDomains';
+export { default as DomainSettings } from '@components/DomainSettings';
+import './lib.css';
 
 export {
     ShortAddress,
@@ -11,4 +11,3 @@ export {
     NetworkLogo,
     YearCounter,
 } from '@components/index';
-export { BaseLayout,UserAuthState,OwnedDomains,DomainSettings };
