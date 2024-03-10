dd915a0466f2163a2f3ea02236192a51c4b06bc7
Yelo <me@yelo.cc>
2024-03-10 22:57:30 +0800

    fix: desktop style of ParkingMode

--- src/components/Templates/ParkingMode/components/NamePotential.tsx
+++ src/components/Templates/ParkingMode/components/NamePotential.tsx
@@ -28,7 +28,7 @@ export const NamePotential = ({
 }: NamePotentialProps) => (
 	<div
 		className={cn(
-			'w-full px-0 py-0 md:py-28 md:pl-32 md:pr-24 bg-none sm:bg-[#161616] flex flex-col md:flex-row flex-wrap gap-x-20 gap-y-24 justify-between relative',
+			'w-full min-w-full px-0 py-0 md:py-28 md:pl-32 md:pr-24 bg-none sm:bg-[#161616] flex flex-col md:flex-row flex-wrap gap-x-20 gap-y-24 justify-between relative',
 			className,
 		)}>
 		<div className="flex-1 pt-40 md:pt-28 px-8 md:px-0 bg-[#161616] md:bg-none">
