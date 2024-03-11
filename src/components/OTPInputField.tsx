34b89ac098c94923f9bbc4c5d22fb3bfda902b47
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-03-11 14:45:20 +0100

    (otp inputfield component): working on the auto completion

--- src/components/OTPInputField.tsx
+++ src/components/OTPInputField.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useRef, useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import { cn } from '../utils/cn'
 
 interface OTPInputFieldProps extends React.ComponentProps<'input'> {
@@ -8,9 +8,7 @@ interface OTPInputFieldProps extends React.ComponentProps<'input'> {
 export const OTPInputField = ({fieldLength = 7, ...props}: OTPInputFieldProps) => {
     const [otp, setOtp] = useState('')
 
-    const REGEX_DIGIT = /^\d/
-
-    // const inputRef = useRef<HTMLInputElement[]>([])
+    const REGEX_DIGIT = /^\d+$/
 
     const otpArray = useMemo(() => {
       const valueArray = otp.split('')
@@ -28,57 +26,59 @@ export const OTPInputField = ({fieldLength = 7, ...props}: OTPInputFieldProps) =
       return items
     }, [otp, fieldLength])
 
+
     const onInputChange = (e:React.ChangeEvent<HTMLInputElement>, index: number) => {
-        // const [start, ...rest] = input
-        // value = start ?? ''
+
         const target = e.target;
-        const value = target.value;
-        if (!REGEX_DIGIT.test(value)) {
+        let value = target.value;
+        const isValue = REGEX_DIGIT.test(value)
+        if (!isValue && value !== '') {
           return;
         }
 
+        value = isValue ? value : ''
+
         const newValue = otp.substring(0, index) + value + otp.substring(index + 1)
         setOtp(newValue)
+
+        if (!isValue) {
+          return
+        }
 
         const nextElementSibling = target.nextElementSibling as HTMLInputElement
         if (nextElementSibling) {
             nextElementSibling.focus()
             
         }
-        // const newPin = [...otp]
-        // newPin[index] = input
-        // setOtp(newPin)
-        // if (input && index < fieldLength - 1) {
-          // inputRef.current[index + 1].focus()
-          // // inputRef.current[index + 1].value = otp.join()
-          // inputRef.current[index + 1].dispatchEvent(new Event('change'))
-        // }
     }
 
-    // const handleKeyDown = useCallback((e:any, index: number) => {
-    //     if (e.keyCode === 8 && e.target.value === '') {
-    //       inputRef.current[index - 1].focus()
-    //     }
-    // }, [otp])
+    const onKeyDown = (e:React.KeyboardEvent<HTMLInputElement>) => {
+      const target = e.target as HTMLInputElement
+      const previousElementSibling = target.previousElementSibling as HTMLInputElement
+        if (( e.keyCode !== 8 || e.key !== 'Backspace') || target.value !== '') {
+            return
+        }
+        if (previousElementSibling) {
+          previousElementSibling.focus()
+        }
+    }
   return (
     // <div className='w-full flex gap-1'>
     <>
         {
             otpArray.map((digit, index) => (
                 <input 
-                
                     className={cn(' outline-none w-7 h-9 rounded-2 placeholder:text-primary-500 focus:border focus:bg-black-900 focus:border-primary-500 bg-[#313131cc] font-primary tracking-wider font-normal text-lg text-center text-primary-500 ',otp[index] && ' bg-black-900' )}
                     placeholder='_'
                     type="text"
                     inputMode='numeric'
                     value={digit}
                     pattern='\d{1}'
-                    maxLength={7}
-                    // onKeyDown={e =>handleKeyDown(e, index)}
+                    maxLength={fieldLength}
+                    onKeyDown={evt => onKeyDown(evt)}
                     onChange={evt => onInputChange(evt, index)}
-                    // ref={element => inputRef.current[index] = element as HTMLInputElement}
                     key={`input-${index}`} 
-                    {...props}/>
+                    />
             ) )
         }
     </>
