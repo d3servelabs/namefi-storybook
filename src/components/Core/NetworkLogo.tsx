462a3e603c113c1b44a458554d193090fc5bd591
samishal1998 <samishal.1998@gmail.com>
2024-03-14 02:50:49 +0200

    feat: added base network logo

--- src/components/Core/NetworkLogo.tsx
+++ src/components/Core/NetworkLogo.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { cn } from '../../utils/cn';
 import { EthereumLogo } from './icons/EthereumLogo';
+import * as net from 'net';
+import PolygonNetwork from './icons/PolygonNetwork';
+import BaseNetwork from './icons/BaseNetwork';
+import EthNetwork from './icons/EthNetwork';
 
 export const NetworkLogo = ({
 	network,
@@ -8,18 +12,26 @@ export const NetworkLogo = ({
 }: React.ComponentProps<'div'> & {
 	network: number;
 }) => {
-	//todo
+	const testnet = React.useMemo(() => {
+		return [11155111, 5, 80001, 84531].includes(network as any);
+	}, [network]);
 	return (
 		<div
 			{...props}
-			className={cn('w-12 h-12 aspect-square rounded-full text-center', props.className)}
+			className={cn(
+				'w-12 h-12 aspect-square rounded-full text-center',
+				testnet && 'grayscale',
+				props.className,
+			)}
 			style={{
 				background: '#617DEA',
 			}}>
-			{network === 1 ? (
-				<EthereumLogo className={'w-12 h-12'} />
-			) : [1, 11155111, 5].includes(network) ? (
-				<EthereumLogo className={'w-12 h-12 grayscale-1'} />
+			{[1, 11155111, 5].includes(network as any) ? (
+				<EthNetwork className={cn('w-full h-full')} />
+			) : [137, 80001].includes(network as any) ? (
+				<PolygonNetwork className={cn('w-full h-full')} />
+			) : [8453, 84531].includes(network as any) ? (
+				<BaseNetwork className={cn('w-full h-full')} />
 			) : (
 				<div className={'grid place-items-center h-full w-full'}>
 					<span className={'text-2xl font-bold text-slate-100/90'}>?</span>
--- src/components/Core/icons/index.tsx
+++ src/components/Core/icons/index.tsx
@@ -1,4 +1,5 @@
 export { default as AvalancheNetwork } from './AvalancheNetwork';
+export { default as BaseNetwork } from './BaseNetwork';
 export { default as EthNetwork } from './EthNetwork';
 export { default as PolygonNetwork } from './PolygonNetwork';
 export { default as StacksNetwork } from './StacksNetwork';
