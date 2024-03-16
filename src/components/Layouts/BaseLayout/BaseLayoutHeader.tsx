a30ecc8f41213148622bb683d92ac615b5563cc3
samishal1998 <samishal.1998@gmail.com>
2024-03-17 00:43:08 +0200

    feat: header logo is a link to base path

--- src/components/Layouts/BaseLayout/BaseLayoutHeader.tsx
+++ src/components/Layouts/BaseLayout/BaseLayoutHeader.tsx
@@ -4,13 +4,14 @@ import {cn} from "../../../utils/cn";
 type AppLayoutHeaderProps = React.ComponentProps<'header'> & {
 	content?: ReactNode | undefined;
 	endSlot?: ReactNode;
+	startSlot?: ReactNode;
 };
 
-export default function BaseLayoutHeader({ content, endSlot,className }: AppLayoutHeaderProps) {
+export default function BaseLayoutHeader({ content,startSlot = <a href={'/'}><NamefiLogo className={"w-22.5"}/></a>, endSlot,className }: AppLayoutHeaderProps) {
 	return (
 		<header className={cn("namefi-header relative h-20 min-h-20 pl-6 pr-3 rounded-full w-full m-auto mt-9",className)}>
 			<nav className="relative w-full h-full m-0 flex justify-between items-center">
-				<NamefiLogo className={"w-22.5"}/>
+				<>{startSlot}</>
 				<>{content}</>
 				<div>{endSlot}</div>
 			</nav>
