5e6e0b2d4c1edb58be10b78930c45bdb5d08c879
Yelo <me@yelo.cc>
2024-03-31 04:15:38 +0800

    feat(importflow): use NetworkSelect in ImportFlow

--- src/components/ImportFlow/views/GetStarted.tsx
+++ src/components/ImportFlow/views/GetStarted.tsx
@@ -5,16 +5,16 @@ import { NamefiBrandText } from '../../../components';
 import { Balance } from '../../../components/Core/Balance';
 import { ImportFlowLayout } from '../components/ImportFlowLayout';
 import { ActionButton } from '../components/ActionButton';
+import { NetworkSelect, type NetworkOption } from '../components/NetworkSelect'
 
-export interface Option {
-	label: string;
-	value: string;
-}
+export type { NetworkOption };
 
 export interface GetStartedProps {
 	costValue: number;
 	costExchangeRate?: number;
-	networkOptions?: Option[];
+	network?: string;
+	networkOptions?: NetworkOption[];
+	onChangeNetwork?: (network: string) => void;
 	onClickStart?: () => void;
 	onBack?: () => void;
 	className?: string;
@@ -23,7 +23,9 @@ export interface GetStartedProps {
 export const GetStarted = ({
 	costValue,
 	costExchangeRate = 1,
+	network,
 	networkOptions,
+	onChangeNetwork,
 	onClickStart,
 	onBack,
 	className,
@@ -37,7 +39,7 @@ export const GetStarted = ({
 			}
 			description="Mint your domain NFT and start trading it hassle-free."
 			actions={
-				<ActionButton icon={<ArrowRightIcon />} className="w-auto" onClick={onClickStart}>
+				<ActionButton icon={<ArrowRightIcon />} className="w-auto" onClick={onClickStart} disabled={!!networkOptions?.length && !network}>
 					Start Import
 				</ActionButton>
 			}
@@ -57,7 +59,7 @@ export const GetStarted = ({
 				</ImportFlowLayout.Field>
 				{!!networkOptions?.length && (
 					<ImportFlowLayout.Field label="I'd like to import to">
-						Select Network
+						<NetworkSelect value={network} onChange={onChangeNetwork} options={networkOptions} />
 					</ImportFlowLayout.Field>
 				)}
 			</div>
