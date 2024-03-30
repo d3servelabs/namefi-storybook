886a7d382319496f65a2ce3984839fb11d6e5dee
Yelo <me@yelo.cc>
2024-03-31 01:42:15 +0800

    feat: add pending prop for Steps component

--- src/components/DomainOwned/components/Steps.tsx
+++ src/components/DomainOwned/components/Steps.tsx
@@ -5,11 +5,12 @@ import { cn } from '../../../utils/cn';
 export interface StepsProps {
 	current: number;
 	items: React.ReactNode[];
+	pending?: React.ReactNode;
 	itemClassName?: string;
 	className?: string;
 }
 
-const stylesheet = css`
+const stylesheets = css`
 	& > :first-child {
 		position: relative;
 		&:before,
@@ -17,10 +18,9 @@ const stylesheet = css`
 			content: '';
 			position: absolute;
 			width: 50%;
-			height: 1.5px;
-			background: #c6eedb;
+			border-top-width: 1px;
+			border-color: #e6eedb;
 			top: 50%;
-			opacity: 0.1;
 			z-index: -1;
 		}
 		&:before {
@@ -28,6 +28,14 @@ const stylesheet = css`
 		}
 		&:after {
 			left: 50%;
+		}
+		& > :nth-child(2) {
+			position: absolute;
+			left: 50%;
+			width: 100%;
+			font-size: 10px;
+			display: flex;
+			justify-content: center;
 		}
 	}
 	& > :last-child {
@@ -49,15 +57,21 @@ const stylesheet = css`
 	}
 `;
 
-export const Steps = ({ items, current, itemClassName, className }: StepsProps) => {
+export const Steps = ({ items, current, pending, itemClassName, className }: StepsProps) => {
 	return (
 		<ol className={cn('flex', className)}>
 			{items.map((item, index) => {
 				return (
 					<li
 						key={index}
-						className={cn('flex flex-col items-center gap-y-3', stylesheet)}>
-						<div className="w-full flex justify-center">
+						className={cn('flex flex-col items-center gap-y-3', stylesheets)}>
+						<div
+							className={cn(
+								'w-full flex justify-center',
+								'before:border-solid after:border-solid before:opacity-20 after:opacity-20',
+								index === current - 0.5 && 'after:border-dashed after:opacity-90',
+								index === current + 0.5 && 'before:border-dashed before:opacity-90',
+							)}>
 							<div
 								className={cn(
 									'w-[28px] y-[28px] border border-primary-dark rounded-full flex items-center justify-center text-base font-semibold',
@@ -67,6 +81,7 @@ export const Steps = ({ items, current, itemClassName, className }: StepsProps) 
 								)}>
 								{index + 1}
 							</div>
+							{index === current - 0.5 && pending}
 						</div>
 						<div
 							className={cn(
