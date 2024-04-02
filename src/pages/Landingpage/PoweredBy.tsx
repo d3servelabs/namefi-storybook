051f0e4db1d21e201a0746a05d52766c1527755c
samishal1998 <samishal.1998@gmail.com>
2024-04-02 23:04:08 +0200

    feat(responsive): fix PoweredBy responsive

--- src/pages/Landingpage/PoweredBy.tsx
+++ src/pages/Landingpage/PoweredBy.tsx
@@ -6,9 +6,9 @@ import awsSvg from '../../../public/assets/aws.svg'
 
 export const PoweredBy = () => {
   return (
-    <div className='w-full xl:w-[1200px] h-[166px] flex flex-col '>
+    <div className='w-full max-w-[1400px] min-h-[166px] flex flex-col'>
         <p className='font-primary text-lg font-normal text-brand-300 tracking-wide self-start '>Powered by</p>
-        <div className='h-[142px] flex gap-10 xl:gap-20 items-center justify-center'>
+        <div className='min-h-[142px] flex flex-wrap gap-10 xl:gap-20 items-center justify-center'>
             <img src={ethImageSvg} alt="" />
             <img src={ethImage1Svg} alt="" />
             <img src={googleCloudSvg} alt="" />
