0ca2c6b32e6712a52a5c66cbd49659a56990a6dc
samishal1998 <samishal.1998@gmail.com>
2024-04-08 17:07:30 +0200

    fix: fix css-in-js, emotion not working.
    had to switch `swc`

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,15 +5,14 @@ import typescript from '@rollup/plugin-typescript';
 import postcss from 'rollup-plugin-postcss';
 import dts from 'rollup-plugin-dts';
 import image from '@rollup/plugin-image';
-import svgr from '@svgr/rollup'
 import {fileURLToPath} from 'url';
 // This is required to read package.json file when
 // using Native ES modules in Node.js
 // https://rollupjs.org/command-line-interface/#importing-package-json
 import {createRequire} from 'node:module';
-import alias from "@rollup/plugin-alias";
 import path from "path";
 import postcssNested from 'postcss-nested';
+import { swc, defineRollupSwcOption } from 'rollup-plugin-swc3';
 
 const requireFile = createRequire(import.meta.url);
 const packageJson = requireFile('./package.json');
@@ -21,12 +20,11 @@ const packageJson = requireFile('./package.json');
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 import tailwindcss from 'tailwindcss';
-
 import tailwindConfig from './tailwind.config.js';
+import svgr from "vite-plugin-svgr";
 
 export default [
     {
-        important: '#v2',
         input: 'src/index.ts',
         output: [
             {
@@ -40,11 +38,24 @@ export default [
                 sourcemap: true,
             },
         ],
+
         plugins: [
             peerDepsExternal(),
             resolve(),
-            svgr({icon: true, memo: true, exportType: "default",}),
             commonjs(),
+            swc(
+                defineRollupSwcOption({
+                    include: /\.[jt]sx?$/,
+                    exclude: /(node_modules|lib)/,
+                    tsconfig: 'tsconfig.lib.json',
+                    sourceMaps:true,
+                    cwd: __dirname,
+                    jsc:{
+                        baseUrl: __dirname,
+                    }
+                })
+            ),
+            svgr({icon: true, memo: true, exportType: "default",}),
             postcss({
                 extensions: ['.css'],
                 plugins: [
@@ -60,7 +71,8 @@ export default [
                     })],
                 extract: true,  // extracts all css to 'lib/styles.css'
             }),
-            typescript({tsconfig: './tsconfig.lib.json',}),
+
+            typescript({tsconfig: './tsconfig.lib.json', sourceMap: false}),
             image(),
         ],
         external: [
