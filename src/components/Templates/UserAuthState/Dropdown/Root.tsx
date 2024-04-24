ac841228c0087a3fc97aa8e04c66e3ea1ebbafac
samishal1998 <samishal.1998@gmail.com>
2024-04-25 01:59:53 +0200

    fix: wrong padding on wallet dropdown

--- src/components/Templates/UserAuthState/Dropdown/Root.tsx
+++ src/components/Templates/UserAuthState/Dropdown/Root.tsx
@@ -9,7 +9,7 @@ const Root = React.forwardRef<HTMLDivElement, UserAuthStateDropdownProps>(
 				{...props}
 				ref={ref}
 				className={cn(
-					'relative rounded-[2rem] bg-[#111] drop-shadow-[0_0_20px_rgba(255,239,239,0.15)] py-12 px-6 lg:max-w-[515px] min-w-[460px] flex flex-col ',
+					'relative rounded-[2rem] bg-[#111] drop-shadow-[0_0_20px_rgba(255,239,239,0.15)] py-6 px-6 max-w-[100%] lg:max-w-[515px] min-w-[460px] flex flex-col ',
 					props.className,
 				)}>
 				{children}
