cc02e810fcdabda17b0da4a01ec32be5db8034b3
samishal1998 <samishal.1998@gmail.com>
2024-04-25 03:31:06 +0200

    fix: vite tailwind nested css for storybook

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,12 @@
 const tailwindcss = require('tailwindcss');
+const tailwindcssNesting = require('tailwindcss/nesting');
 const autoprefixer = require('autoprefixer');
 const postcssNested = require('postcss-nested');
 const postcss = require('postcss');
 
 module.exports = postcss(
 	postcssNested(),
+	tailwindcssNesting(),
 	tailwindcss(),
 	autoprefixer(),
 );
