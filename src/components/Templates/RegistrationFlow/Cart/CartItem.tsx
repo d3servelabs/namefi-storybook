97f09c71a49100b630dff238f0fbc53ad749073f
samishal1998 <samishal.1998@gmail.com>
2024-04-26 17:44:51 +0300

    fix: 86a35zhx1 - remove trash icon

--- src/components/Templates/RegistrationFlow/Cart/CartItem.tsx
+++ src/components/Templates/RegistrationFlow/Cart/CartItem.tsx
@@ -1,31 +1,50 @@
-import {GhostButton} from '../../../Core';
-import {TrashIcon} from '@radix-ui/react-icons';
-import React from 'react';
+import { GhostButton } from '../../../Core';
+import { TrashIcon } from '@radix-ui/react-icons';
+import React, { ComponentProps } from 'react';
+import { cn } from '../../../../utils/cn';
 
-export default function CartItem({domainName, years, price}) {
-    return (
-        <>
-            <div className="relative w-full flex justify-between">
-                <div
-                    className="w-[120px] h-[120px] bg-brand-black rounded-lg drop-shadow-[0_2px_4px_rgba(202,202,202,0.25)] flex justify-center items-center">
-                    <p className="font-bold text-xs tracking-widest text-white">{domainName}</p>
-                </div>
-                <div className="w-[332px] flex justify-between items-start text-white font-primary font-normal ">
-                    <div className="min-w-[109px] flex flex-col gap-2  ">
-                        <p>{domainName}</p>
-                        <p>{years} Year</p>
-                    </div>
-                    <div className="relative flex gap-6 items-center ">
-                        <p className="flex gap-3 text-sm tracking-[0.02em]  ">
-                            <span>{price}</span>
-                            <span>$NFSC</span>
-                        </p>
-                        <GhostButton className={'rounded-full aspect-square w-7 h-7 p-1'}>
-                            <TrashIcon className={'text-primary w-5 h-5'}/>
-                        </GhostButton>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+export default function CartItem({
+	domainName,
+	years,
+	price,
+	trashButton,
+}: {
+	domainName: string;
+	years: string[];
+	price: number;
+	trashButton?: React.ReactNode;
+}) {
+	return (
+		<>
+			<div className="relative w-full flex justify-between">
+				<div className="w-[120px] h-[120px] bg-brand-black rounded-lg drop-shadow-[0_2px_4px_rgba(202,202,202,0.25)] flex justify-center items-center">
+					<p className="font-bold text-xs tracking-widest text-white">{domainName}</p>
+				</div>
+				<div className="w-[332px] flex justify-between items-start text-white font-primary font-normal ">
+					<div className="min-w-[109px] flex flex-col gap-2  ">
+						<p>{domainName}</p>
+						<p>{years} Year</p>
+					</div>
+					<div className="relative flex gap-6 items-center ">
+						<p className="flex gap-3 text-sm tracking-[0.02em]">
+							<span>{price}</span>
+							<span>$NFSC</span>
+						</p>
+						{trashButton}
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
+CartItem.TrashButton = TrashButton;
+
+function TrashButton(props: ComponentProps<typeof GhostButton>) {
+	return (
+		<GhostButton
+			{...props}
+			className={cn(props.className, 'rounded-full aspect-square w-7 h-7 p-1')}>
+			<TrashIcon className={'text-primary w-5 h-5'} />
+		</GhostButton>
+	);
 }
