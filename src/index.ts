a8beb66214f0f651eb9714c4943c0d4dd059f0e0
samishal1998 <samishal.1998@gmail.com>
2024-04-26 17:21:30 +0300

    fix: 86a35xrur - typography letter-spacing mismatch

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export { default as OwnedDomains } from './components/Templates/OwnedDomains';
 export { default as DomainSettings } from './components/Templates/DomainSettings';
 export * as LandingPage from './pages/Landingpage';
 import './lib.css';
-import './App.css';
+
 
 export * from './components';
 
