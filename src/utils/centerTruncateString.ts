4279ccb10def74b4da35017443e636105b9c692c
samishal1998 <samishal.1998@gmail.com>
2024-04-26 17:54:06 +0300

    fix: add util to truncate strong from center

---
+++ src/utils/centerTruncateString.ts
@@ -0,0 +1,11 @@
+export default function centerTruncateString(str: string, length: number,filler= "...") {
+    if(str.length > length) {
+        const charsLength = str.length - length - filler.length;
+        const firstPartLength = Math.floor(charsLength/2);
+        const secondPartLength = charsLength - firstPartLength;
+
+        return str.substring(0, firstPartLength+1) + filler + str.substring(str.length-secondPartLength);
+    }
+    return str;
+
+}
