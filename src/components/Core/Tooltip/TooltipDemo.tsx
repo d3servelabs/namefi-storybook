20493f3c8843f296355ee2f81f0e0034701a28ba
samishal1998 <samishal.1998@gmail.com>
2024-05-01 05:13:19 +0300

    hotfix: remove radix-ui, causing build issues on the other side

--- src/components/Core/Tooltip/TooltipDemo.tsx
+++ src/components/Core/Tooltip/TooltipDemo.tsx
@@ -1,37 +1,36 @@
 import React from 'react'
 import ToolTips from '.'
-import * as Tooltip from '@radix-ui/react-tooltip';
-import { TooltipImage } from './TooltipImage';
+//import { TooltipImage } from './TooltipImage';
 export const TooltipDemo = () => {
 
-  return (
-    <Tooltip.Provider>
-            <Tooltip.Root >
-                <Tooltip.Trigger>
-                        <h6 className='text-white'>Hover to show tooltip</h6>
-                </Tooltip.Trigger>
-                    <Tooltip.Content sideOffset={5} align='start'>
-                        <ToolTips.Content.Long icon={<TooltipImage />}>
-                            <ToolTips.Info>
-                                <ToolTips.Typography.Title>
-                                    AutoPark™️
-                                </ToolTips.Typography.Title>
-                                <ToolTips.Typography.BodyText>
-                                    <p>Your domain is automatically set to <ToolTips.Typography.Span>parking mode</ToolTips.Typography.Span> - for sale.</p>
-                                    <p>Visitors can <ToolTips.Typography.Span>buy</ToolTips.Typography.Span> or <ToolTips.Typography.Span>make an offer</ToolTips.Typography.Span> to you.</p>
-                                </ToolTips.Typography.BodyText>
-                                <ToolTips.Typography.Link href='#'>
-                                    Learn More
-                                </ToolTips.Typography.Link>
-                            </ToolTips.Info>
-                        </ToolTips.Content.Long>
-                        {/* <ToolTips.Content.Short>
-                            <ToolTips.Typography.BodyText>
-                                To change type, please delete and add new record
-                            </ToolTips.Typography.BodyText>
-                        </ToolTips.Content.Short> */}
-                    </Tooltip.Content>
-            </Tooltip.Root>
-    </Tooltip.Provider>
+  return (<></>
+    // <Tooltip.Provider>
+    //         <Tooltip.Root >
+    //             <Tooltip.Trigger>
+    //                     <h6 className='text-white'>Hover to show tooltip</h6>
+    //             </Tooltip.Trigger>
+    //                 <Tooltip.Content sideOffset={5} align='start'>
+    //                     <ToolTips.Content.Long icon={<TooltipImage />}>
+    //                         <ToolTips.Info>
+    //                             <ToolTips.Typography.Title>
+    //                                 AutoPark™️
+    //                             </ToolTips.Typography.Title>
+    //                             <ToolTips.Typography.BodyText>
+    //                                 <p>Your domain is automatically set to <ToolTips.Typography.Span>parking mode</ToolTips.Typography.Span> - for sale.</p>
+    //                                 <p>Visitors can <ToolTips.Typography.Span>buy</ToolTips.Typography.Span> or <ToolTips.Typography.Span>make an offer</ToolTips.Typography.Span> to you.</p>
+    //                             </ToolTips.Typography.BodyText>
+    //                             <ToolTips.Typography.Link href='#'>
+    //                                 Learn More
+    //                             </ToolTips.Typography.Link>
+    //                         </ToolTips.Info>
+    //                     </ToolTips.Content.Long>
+    //                     {/* <ToolTips.Content.Short>
+    //                         <ToolTips.Typography.BodyText>
+    //                             To change type, please delete and add new record
+    //                         </ToolTips.Typography.BodyText>
+    //                     </ToolTips.Content.Short> */}
+    //                 </Tooltip.Content>
+    //         </Tooltip.Root>
+    // </Tooltip.Provider>
   )
 }
--- src/components/Templates/UserAuthState/UserAuthStateDemo.tsx
+++ src/components/Templates/UserAuthState/UserAuthStateDemo.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo } from 'react';
 import  ShortAddress  from '../../Core/ShortAddress';
 import  UserAuthStateTile from './Tile';
-import { Popover } from '@radix-ui/themes';
+// import { Popover } from '@radix-ui/themes';
 import UserAuthStateDropDown from './Dropdown'; 
 
 
@@ -26,54 +26,54 @@ export const UserAuthStateDemo = ({
 }: UserAuthStateProps) => {
 	return (
 		<>
-			<Popover.Root>
-				<Popover.Trigger>
-					<UserAuthStateTile.Root backgroundColor={userAddress ? '#111' : '#111111b3'}>
-						<UserAuthStateTile.Network network={1} onClick={onNetworkClicked} />
-						<UserAuthStateTile.Label>
-							{userLoading || balanceLoading ? (
-								'Loading...'
-							) : networkError ? (
-								'Wrong Network'
-							) : userAddress ? (
-								<ShortAddress address={userAddress} />
-							) : (
-								'Connect Wallet'
-							)}
-						</UserAuthStateTile.Label>
-						<UserAuthStateTile.Action>
-							{userLoading || balanceLoading || networkError ? (
-								<img
-									className="w-[22px]"
-									src="/assets/arrow-right1.svg"
-									alt="arrow-right1 logo"
-									role="img"
-								/>
-							) : (
-								<img
-									onClick={(event)=>{
-										event.stopPropagation()
-										if(userAddress)
-											onSignOut?.()
-										else onLogin?.()
-									}}
-									src={userAddress ? '/assets/disconnect.svg' : '/assets/arrow-right1.svg'}
-									className={userAddress ? 'max-w-[44px]' : 'max-w-[22px] mr-3'}
-									alt="disconnect image"
-									role="svg"
-								/>
-							)}
-						</UserAuthStateTile.Action>
-					</UserAuthStateTile.Root>
-				</Popover.Trigger>
-				<Popover.Content>
-					<UserAuthStateDropDown.Root>
-						{/* <UserAuthStateDropDown.Header userAddress={userAddress} loading={userLoading}/>
-						<UserAuthStateDropDown.Body/>
-						<UserAuthStateDropDown.Footer/> */}
-					</UserAuthStateDropDown.Root>
-				</Popover.Content>
-			</Popover.Root>
+			{/*<Popover.Root>*/}
+			{/*	<Popover.Trigger>*/}
+			{/*		<UserAuthStateTile.Root backgroundColor={userAddress ? '#111' : '#111111b3'}>*/}
+			{/*			<UserAuthStateTile.Network network={1} onClick={onNetworkClicked} />*/}
+			{/*			<UserAuthStateTile.Label>*/}
+			{/*				{userLoading || balanceLoading ? (*/}
+			{/*					'Loading...'*/}
+			{/*				) : networkError ? (*/}
+			{/*					'Wrong Network'*/}
+			{/*				) : userAddress ? (*/}
+			{/*					<ShortAddress address={userAddress} />*/}
+			{/*				) : (*/}
+			{/*					'Connect Wallet'*/}
+			{/*				)}*/}
+			{/*			</UserAuthStateTile.Label>*/}
+			{/*			<UserAuthStateTile.Action>*/}
+			{/*				{userLoading || balanceLoading || networkError ? (*/}
+			{/*					<img*/}
+			{/*						className="w-[22px]"*/}
+			{/*						src="/assets/arrow-right1.svg"*/}
+			{/*						alt="arrow-right1 logo"*/}
+			{/*						role="img"*/}
+			{/*					/>*/}
+			{/*				) : (*/}
+			{/*					<img*/}
+			{/*						onClick={(event)=>{*/}
+			{/*							event.stopPropagation()*/}
+			{/*							if(userAddress)*/}
+			{/*								onSignOut?.()*/}
+			{/*							else onLogin?.()*/}
+			{/*						}}*/}
+			{/*						src={userAddress ? '/assets/disconnect.svg' : '/assets/arrow-right1.svg'}*/}
+			{/*						className={userAddress ? 'max-w-[44px]' : 'max-w-[22px] mr-3'}*/}
+			{/*						alt="disconnect image"*/}
+			{/*						role="svg"*/}
+			{/*					/>*/}
+			{/*				)}*/}
+			{/*			</UserAuthStateTile.Action>*/}
+			{/*		</UserAuthStateTile.Root>*/}
+			{/*	</Popover.Trigger>*/}
+			{/*	<Popover.Content>*/}
+			{/*		<UserAuthStateDropDown.Root>*/}
+			{/*			/!* <UserAuthStateDropDown.Header userAddress={userAddress} loading={userLoading}/>*/}
+			{/*			<UserAuthStateDropDown.Body/>*/}
+			{/*			<UserAuthStateDropDown.Footer/> *!/*/}
+			{/*		</UserAuthStateDropDown.Root>*/}
+			{/*	</Popover.Content>*/}
+			{/*</Popover.Root>*/}
 		</>
 	);
 };
