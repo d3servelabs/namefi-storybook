1b69022bfde4bb3b7cb4f6bded7687c564d39f92
Yelo <me@yelo.cc>
2024-05-03 23:24:29 +0800

    refactor(landingpage): update supporting layout

--- src/pages/LandingPage/sections/Supporting.tsx
+++ src/pages/LandingPage/sections/Supporting.tsx
@@ -17,7 +17,7 @@ export const Supporting = ({ items = [], className }: SupportingProps) => {
 			<div className="text-brand-300 text-[10px] md:text-lg text-center md:text-left font-normal font-primary tracking-wide mb-8 md:mb-4">
 				Proudly supporting
 			</div>
-			<div className="flex flex-wrap justify-center gap-3 p-0 md:p-2.5 items-center">
+			<div className="flex md:grid grid-cols-[repeat(auto-fit,minmax(160px,1fr))] flex-wrap justify-center gap-3 p-0 md:p-2.5 items-center">
 				{items.map(({ name, logo }, index) => (
 					<div className="flex justify-center items-center h-15 md:h-20 w-full md:w-auto">
 						<img key={index} className="w-60" src={logo} alt={name} />
