3115dc7ae2366ebaadeabbd7887dadf813ab1a17
samishal1998 <samishal.1998@gmail.com>
2024-05-04 05:26:58 +0300

    feat: added an option to change surface to error state

--- src/components/Core/Surface/index.tsx
+++ src/components/Core/Surface/index.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 import { cn } from '../../../utils/cn';
 
 export interface SurfaceProps {
-    children?: React.ReactNode;
-    className?: string;
+	error?: boolean;
+	children?: React.ReactNode;
+	className?: string;
 }
-const Surface = ({ children, className }: SurfaceProps) => {
-    return (
-        <div
-            className={cn(
-                'w-full h-full rounded-5 bg-primary-5 overflow-hidden',
-                className,
-            )}>
-            {children}
-        </div>
-    );
+
+const Surface = ({ error, children, className }: SurfaceProps) => {
+	return (
+		<div
+			className={cn(
+				'w-full h-full rounded-5 overflow-hidden',
+				error ? 'bg-error-5' : 'bg-primary-5',
+				className,
+			)}>
+			{children}
+		</div>
+	);
 };
 
-export default Surface
+export default Surface;
