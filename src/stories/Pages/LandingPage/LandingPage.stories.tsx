1b27d1ee9c689cabbecca7b12ab6389f6fccee85
Yelo <me@yelo.cc>
2024-05-08 03:09:59 +0800

    feat(landingpage): optimize ui for faq title

--- src/stories/Pages/LandingPage/LandingPage.stories.tsx
+++ src/stories/Pages/LandingPage/LandingPage.stories.tsx
@@ -243,7 +243,12 @@ export const FullPage: Story = {
 		],
 		faqs: [
 			{
-				question: 'What is Namefi??',
+				question: (
+					<>
+						Does <NamefiBrandText className="text-[1em]">Namefi</NamefiBrandText>{' '}
+						support ENS, Handshake, Unstoppable Domain, and other web3 names?
+					</>
+				),
 				answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 			},
 			{
