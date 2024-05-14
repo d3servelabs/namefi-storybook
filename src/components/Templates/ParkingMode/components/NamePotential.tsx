f945d37e0c72b7d75966fe45fac6b67b9059cc9f
Yelo <me@yelo.cc>
2024-05-14 23:42:54 +0800

    fix(parkingmode): remove unused import

--- src/components/Templates/ParkingMode/components/NamePotential.tsx
+++ src/components/Templates/ParkingMode/components/NamePotential.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from '@emotion/styled';
 import { cn } from '../../../../utils/cn';
 import { PhoneMockup } from './PhoneMockup';
 import { Markdown } from './Markdown';
