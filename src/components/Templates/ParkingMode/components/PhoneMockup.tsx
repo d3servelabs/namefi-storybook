b16f0ecd6a24abf989ad00dbf54b1551fb9e7fe5
Yelo <me@yelo.cc>
2024-05-18 17:39:42 +0800

    fix(parkingmode): inline svg in css

--- src/components/Templates/ParkingMode/components/PhoneMockup.tsx
+++ src/components/Templates/ParkingMode/components/PhoneMockup.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import { css } from '@emotion/css';
 import { cn } from '../../../../utils/cn';
-import iPhoneMaskSVG from '../../../../assets/iPhoneMask.svg';
-import iPhoneMaskFullSVG from '../../../../assets/iPhoneMaskFull.svg';
-import iPhoneSVG from '../../../../assets/iPhone.svg';
-import StyledDiv from "../../../Core/StyledDiv";
-import {css} from "@emotion/css";
+import iPhoneMaskSVG from '../../../../assets/iPhoneMask.svg?raw';
+import iPhoneMaskFullSVG from '../../../../assets/iPhoneMaskFull.svg?raw';
+import iPhoneSVG from '../../../../assets/iPhone.svg?raw';
+import StyledDiv from '../../../Core/StyledDiv';
 
 const PhoneMockupContent = StyledDiv(css`
 	position: absolute;
@@ -12,11 +12,11 @@ const PhoneMockupContent = StyledDiv(css`
 	left: 0;
 	width: 100%;
 	height: 100%;
-	mask-image: url(${iPhoneMaskFullSVG});
+	mask-image: url('data:image/svg+xml,${encodeURIComponent(iPhoneMaskFullSVG)}');
 	mask-size: 100%;
 	mask-repeat: no-repeat;
 	@media (min-width: 640px) {
-		mask-image: url(${iPhoneMaskSVG});
+		mask-image: url('data:image/svg+xml,${encodeURIComponent(iPhoneMaskSVG)}');
 	}
 `);
 
@@ -30,14 +30,16 @@ export const PhoneMockup = ({ children, className }: PhoneMockupProps) => {
 		<div className={cn('relative', className)}>
 			<img
 				className="hidden sm:block w-screen sm:w-[373px] sm:min-w-[373px]"
-				src={iPhoneSVG}
+				src={`data:image/svg+xml,${encodeURIComponent(iPhoneSVG)}`}
 			/>
 			<img
 				className="block sm:hidden w-screen"
-				src={iPhoneMaskFullSVG}
+				src={`data:image/svg+xml,${encodeURIComponent(iPhoneMaskFullSVG)}`}
 			/>
 			<PhoneMockupContent>
-				<div className="w-full h-full py-0 px-0 sm:py-3 sm:px-4 bg-[#2d2d2d]">{children}</div>
+				<div className="w-full h-full py-0 px-0 sm:py-3 sm:px-4 bg-[#2d2d2d]">
+					{children}
+				</div>
 			</PhoneMockupContent>
 		</div>
 	);
