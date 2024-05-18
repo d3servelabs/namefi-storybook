eaaa24cc82659101ef7b8c1ef07053823e68f42d
Yelo <me@yelo.cc>
2024-05-18 17:39:42 +0800

    fix(parkingmode): inline svg in css

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,17 +1,21 @@
 /// <reference types="vite-plugin-svgr/client" />
 
-declare module '*.svg' {
-    const content: any;
-    export default content;
-  }
-
-declare module '*.png' {
-    const content: any;
-    export default content;
+declare module '*?raw' {
+	const content: any;
+	export default content;
 }
 
+declare module '*.svg' {
+	const content: any;
+	export default content;
+}
+
+declare module '*.png' {
+	const content: any;
+	export default content;
+}
 
 declare module '*.module.css' {
-    const classes: { [key: string]: string };
-    export default classes;
+	const classes: { [key: string]: string };
+	export default classes;
 }
