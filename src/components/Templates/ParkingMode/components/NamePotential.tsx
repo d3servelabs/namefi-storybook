ad1bd057c52e4b2b64ca41ca71e98d298143f92b
Yelo <me@yelo.cc>
2024-05-18 22:32:54 +0800

    feat(parkingmode): hide potential preview by default on mobile

--- src/components/Templates/ParkingMode/components/NamePotential.tsx
+++ src/components/Templates/ParkingMode/components/NamePotential.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { cn } from '../../../../utils/cn';
 import { PhoneMockup } from './PhoneMockup';
 import { Markdown } from './Markdown';
-import {css} from "@emotion/css";
+import { css } from '@emotion/css';
 
 export interface NamePotentialProps {
 	name: string;
@@ -16,7 +16,7 @@ export interface NamePotentialProps {
 const markdownClasses = css`
 	font-family: 'Roboto Slab', serif;
 	line-height: 1.5em;
-`
+`;
 
 export const NamePotential = ({
 	name,
@@ -28,15 +28,18 @@ export const NamePotential = ({
 }: NamePotentialProps) => (
 	<div
 		className={cn(
-			'w-full min-w-full px-0 py-0 md:py-28 md:pl-32 md:pr-24 bg-none sm:bg-[#161616] flex flex-col md:flex-row flex-wrap gap-x-20 gap-y-24 justify-between relative',
+			'w-full min-w-full px-0 pt-0 pb-16 md:py-28 md:pl-32 md:pr-24 bg-none sm:bg-[#161616] flex flex-col md:flex-row flex-wrap gap-x-20 gap-y-24 justify-between relative',
 			className,
 		)}>
-		<div className="flex-1 pt-40 md:pt-28 px-8 md:px-0 bg-[#161616] md:bg-none">
+		<div className="flex-1 pt-40 md:pt-28 pb-8 px-8 md:px-0 bg-[#161616] md:bg-none">
 			<div className="text-3xl md:text-6xl text-white font-semibold mb-8 md:mb-9">{name}</div>
-			<Markdown className={cn(markdownClasses,"text-sm md:text-2xl mb-32 md:mb-24")} content={description} />
+			<Markdown
+				className={cn(markdownClasses, 'text-sm md:text-2xl mb-32 md:mb-24')}
+				content={description}
+			/>
 			<div>{children}</div>
 		</div>
-		<div className="m-auto py-8" ref={previewRef}>
+		<div className={cn('m-auto py-8', preview ? 'block' : 'hidden sm:block')} ref={previewRef}>
 			<PhoneMockup>{preview}</PhoneMockup>
 		</div>
 	</div>
