05fa56458263c7b9a6607dcaed3934b13e4e983e
Luis Terrones-Verastegui <lterrones316@gmail.com>
2024-06-03 14:52:08 -0700

    feat: add a Chip core component and two stories for it

---
+++ src/components/Core/Chip.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { cn } from '../../utils/cn';
+
+
+export type ChipProps = React.ComponentProps<'div'> &
+    Partial<{
+        selected?: boolean;
+        onSelect?: () => void;
+    }>;
+
+export function Chip({ selected,
+    onSelect,
+    children,
+    className,
+    ...props
+}: ChipProps) {
+    return (
+        <div
+            className={cn('py-2.5 px-6.25 flex justify-center items-center border-2 rounded-3xl font-primary text-2.5 md:text-lg cursor-pointer', selected ? 'border-primary-500 bg-black-900 text-white' : 'transition border-[#4a4a4a] bg-[#1c1c1c] text-[#d2d2d2] hover:ring-primary-500 hover:ring-2 hover:ring-inset hover:bg-black-900 hover:text-white', className)} onClick={onSelect} {...props}>
+            {children}
+        </div>
+    );
+};
