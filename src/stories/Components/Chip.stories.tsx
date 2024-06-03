dbea5e26015ed54f2b0c8afa4995b2c33a87cf28
Luis Terrones-Verastegui <lterrones316@gmail.com>
2024-06-03 14:52:08 -0700

    feat: add a Chip core component and two stories for it

---
+++ src/stories/Components/Chip.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Chip } from "../../components/Core/Chip";
+
+const meta = {
+    title: 'Components/Chip',
+    component: Chip,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'centered'
+    }
+} satisfies Meta<typeof Chip>
+
+export default meta;
+
+type Story = StoryObj<typeof meta>
+
+export const ChipUnselected: Story = {
+    args: {
+        selected: false,
+        children: "chip"
+    }
+}
+export const ChipSelected: Story = {
+    args: {
+        selected: true,
+        children: "chip"
+    }
+}
