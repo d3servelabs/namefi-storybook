2a7a84cba5884e76936dd27a07371a8e04c12f4b
samishal1998 <samishal.1998@gmail.com>
2024-06-05 03:59:23 +0300

    build: setup eslint with prettier and husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,25 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "plugin:storybook/recommended"
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+	root: true,
+	env: {browser: true, es2020: true},
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:react/jsx-runtime",
+		"plugin:react-hooks/recommended",
+		"plugin:storybook/recommended",
+		"plugin:prettier/recommended",
+		"prettier"
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parserOptions: {ecmaVersion: 'latest', sourceType: 'module'},
+	settings: {react: {version: '18.2'}},
+	plugins: [
+		'react-refresh',
+	],
+	rules: {
+		'react-refresh/only-export-components': [
+			'warn',
+			{allowConstantExport: true},
+		],
+	},
 }
