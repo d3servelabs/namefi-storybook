982e073555bd996d7e443924042a0f77bf599248
samishal1998 <samishal.1998@gmail.com>
2024-06-16 06:08:13 +0300

    fix: className not passed to ActivitiesRoot

--- src/components/Templates/Dashboard/Activities/ActivitiesRoot.tsx
+++ src/components/Templates/Dashboard/Activities/ActivitiesRoot.tsx
@@ -1,10 +1,15 @@
-import React, {ComponentProps} from 'react'
-import {cn} from "../../../../utils/cn";
-export default function ActivitiesRoot ({children,className,...props}: ComponentProps<'div'>) {
+import React, { ComponentProps } from 'react';
+import { cn } from '../../../../utils/cn';
 
-  return (
-    <div {...props} className={cn('relative flex flex-col justify-start h-90 gap-[0.625rem]')}>
-        {children}
-    </div>
-  )
+export default function ActivitiesRoot({ children, className, ...props }: ComponentProps<'div'>) {
+	return (
+		<div
+			{...props}
+			className={cn(
+				'relative flex flex-col items-stretch justify-start h-90 gap-[0.625rem]',
+				className,
+			)}>
+			{children}
+		</div>
+	);
 }
