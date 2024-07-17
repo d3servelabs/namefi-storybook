8e4b60e43ed8dd8ff945907abbe91e6c8e2cb00c
samishal1998 <samishal.1998@gmail.com>
2024-07-17 17:12:47 +0300

    build: fix github pages deployment [NFDEV-60]

--- apps/storybook/vite.config.js
+++ apps/storybook/vite.config.js
@@ -1,28 +1,15 @@
 /* eslint-disable no-undef */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [svgr(),react(),],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-			'@stories': path.resolve(__dirname, './src/stories'),
-			'@utils': path.resolve(__dirname, './src/utils'),
-		},
-	},
 	build: {
-		lib: {
-			entry: path.resolve(__dirname, 'src/index.ts'),
-			name: 'namefi-ui',
-			fileName: 'index',
-		},
 		rollupOptions: {
 			external: ['react'],
-			plugins: [svgr()],
+			plugins: [],
 			output: {
 				globals: {
 					react: 'React',
