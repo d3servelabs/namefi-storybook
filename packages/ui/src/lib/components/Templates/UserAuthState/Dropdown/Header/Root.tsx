454a9ba7abb59b175901c7d5fc8ad3c799339ba3
Luis Terrones-Verastegui <luistv.swe@gmail.com>
2024-11-07 11:39:36 -0800

    fix: remove fixed height from UserAuthState.Dropdown; fixes NFDEV-114

--- packages/ui/src/lib/components/Templates/UserAuthState/Dropdown/Header/Root.tsx
+++ packages/ui/src/lib/components/Templates/UserAuthState/Dropdown/Header/Root.tsx
@@ -3,8 +3,8 @@ import { Tuple } from '../../../../../utils/Tuple';
 
 export default function Root({ children }: { children: Tuple<React.ReactNode, 2> }) {
 	return (
-		<div className="relative w-full h-17 flex flex-row flex-wrap justify-between items-center pb-5">
-			<div className="relative h-full flex items-center gap-2 ">{children[0]}</div>
+		<div className="relative w-full flex flex-row flex-wrap justify-between items-center pb-5">
+			<div className="relative h-full flex items-center gap-2">{children[0]}</div>
 			<div className="h-full flex items-center justify-center gap-2">{children[1]}</div>
 			<div className={'absolute -bottom-0 left-0 w-full h-0.5 bg-border-500'} />
 		</div>
